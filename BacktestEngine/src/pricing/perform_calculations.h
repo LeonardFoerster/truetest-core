e89c33e65d6f89687cfc0eb0430cda5bdabe529c
Leonard Förster <Leonard.Foerster@proton.me>
2025-11-17 21:58:48 +0100

    moved files to pricing

---
+++ BacktestEngine/src/pricing/perform_calculations.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../pricing/black_scholes.h"
+
+#ifndef PERFORM_CALCULATIONS_H
+#define PERFORM_CALCULATIONS_H
+
+void run_manual_calc();
+int run_csv_calc();
+void print_manual_results(const black_scholes& option_data);
+
+#endif
