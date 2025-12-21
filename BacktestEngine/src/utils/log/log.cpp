f6f4f4b031c562f9f3b892b2021ceffb19e081ef
Leonard Förster <Leonard.Foerster@proton.me>
2025-12-21 21:23:04 +0100

    add mkdir method

---
+++ BacktestEngine/src/utils/log/log.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <optional>
+
+#include "../../core/backtest_core.h"
+#include "../../data/db_connection.h"
+
+
+//todo: implement the event "loop"
+
+bool mkdir()
+{
+    std::error_code ec;
+    bool created = std::filesystem::create_directory("Log", ec);
+
+    if (ec) 
+    {
+        std::cerr << "Error while creating directory: " << ec.message() << '\n';
+        return false;  
+    }
+
+    if (created) std::cout << "Directory created successfully\n";
+    else std::cout << "Directory already exists\n";
+    
+    return true;  
+}
+
+
+void log_event(bool s)
+{
+	mkdir();
+
+
+
+
+
+
+
+}
+
